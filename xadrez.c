b280f6203800a9a12b7117f292a623416d693ffd
Marcello Carvalho <98418111+Marcello-Carvalho@users.noreply.github.com>
2025-02-11 14:21:07 +0000

    Implantação do nível novato

--- xadrez.c
+++ xadrez.c
@@ -4,18 +4,63 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+const int BISPO = 5, TORRE = 5, RAINHA = 8;
+
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    int i = 0, resp = 0;
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    do //menu interativo para definir a peça a ser movimentada
+    {
+        printf("\nQual peça deseja movimentar?\n\n");
+        printf("1. Bispo\n");
+        printf("2. Torre\n");
+        printf("3. Rainha\n");
+        printf("4. Sair\n\n");
+        scanf("%d", &resp);
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+            switch (resp)
+            {
+            case 1: //movimentação do bispo
+                printf("\nMovimentando Bispo...\n\n");
+
+                    for (i = 0; i < BISPO; i++)
+                    {
+                        printf("Cima e Direita\n");
+                    }
+                break;
+
+            case 2: //movimentação da torre
+                printf("\nMovimentando Torre...\n\n");
+
+                    for (i = 0; i < TORRE; i++)
+                    {
+                        printf("Direita\n");
+                    }
+                break;
+
+            case 3: //movimentação da rainha
+                printf("\nMovimentando Rainha...\n\n");
+
+                    for (i = 0; i < RAINHA; i++)
+                    {
+                        printf("Esquerda\n");
+                    }
+                break;
+
+            case 4: //opção de saída
+
+                        printf("Saindo...\n");
+                    
+                break;
+            
+            default:
+
+                printf("Opção inválida!\n");
+                break;
+            }
+                  
+    } while (resp != 4);
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
